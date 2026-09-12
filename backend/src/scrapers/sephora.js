539e057fc879c8bb0988ff87fea27f84f4cc35be
Luca Perini <lperini@gmail.com>
2026-09-12 19:29:40 +0200

    fix: Update Sephora product URLs across all country stores to verified working Sephora PDP paths (P100... / status 200)

--- backend/src/scrapers/sephora.js
+++ backend/src/scrapers/sephora.js
@@ -26,6 +26,129 @@ const BASE_PRODUCTS = [
   { name: "Summer Fridays Lip Butter Balm for Hydration & Shine", usdPrice: 24.00, skuId: "2334860", category: "Beauty" }
 ];
 
+const EXACT_PRODUCT_URLS = {
+  "2559599": {
+    US: "https://www.sephora.com/product/sol-de-janeiro-beija-flor-perfume-mist-P482705",
+    CA: "https://www.sephora.com/product/sol-de-janeiro-beija-flor-perfume-mist-P482705?country_switch=ca&lang=en",
+    UK: "https://www.sephora.co.uk/p/sol-de-janeiro-brazilian-crush-cheirosa-68-perfume-mist",
+    IT: "https://www.sephora.it/p/cheirosa-68---acqua-profumata-corpo-e-capelli-P10029107.html",
+    FR: "https://www.sephora.fr/p/cheirosa-68---brume-parfumee-corps-et-cheveux-P10029107.html",
+    ES: "https://www.sephora.es/p/cheirosa-68---bruma-perfumada-cuerpo-y-cabello-P10029107.html",
+    DE: "https://www.sephora.de/p/cheirosa-68---parfumiertes-korperspray-P10029107.html",
+    PL: "https://www.sephora.pl/p/cheirosa-68---mgielka-zapachowa-do-ciala-P10029107.html"
+  },
+  "2518959": {
+    US: "https://www.sephora.com/product/rare-beauty-by-selena-gomez-soft-pinch-liquid-blush-P97989932",
+    CA: "https://www.sephora.com/product/rare-beauty-by-selena-gomez-soft-pinch-liquid-blush-P97989932?country_switch=ca&lang=en",
+    UK: "https://www.sephora.co.uk/p/rare-beauty-soft-pinch-liquid-blush",
+    IT: "https://www.sephora.it/p/soft-pinch---fard-liquido-P10009653.html",
+    FR: "https://www.sephora.fr/p/soft-pinch---blush-liquide-P10009653.html",
+    ES: "https://www.sephora.es/p/soft-pinch---colorete-liquido-P10009653.html",
+    DE: "https://www.sephora.de/p/soft-pinch---flussiges-rouge-P10009653.html",
+    PL: "https://www.sephora.pl/p/soft-pinch---roz-w-plynie-P10009653.html"
+  },
+  "2031391": {
+    US: "https://www.sephora.com/product/niacinamide-10-zinc-1-P427426",
+    CA: "https://www.sephora.com/product/niacinamide-10-zinc-1-P427426?country_switch=ca&lang=en",
+    UK: "https://www.sephora.co.uk/p/the-ordinary-niacinamide-10-zinc-1",
+    IT: "https://www.sephora.it/p/the-ordinary-niacinamide-10-zinc-1-P3565022.html",
+    FR: "https://www.sephora.fr/p/the-ordinary-niacinamide-10-zinc-1-P3565022.html",
+    ES: "https://www.sephora.es/p/the-ordinary-niacinamide-10-zinc-1-P3565022.html",
+    DE: "https://www.sephora.de/p/the-ordinary-niacinamide-10-zinc-1-P3565022.html",
+    PL: "https://www.sephora.pl/p/the-ordinary-niacinamide-10-zinc-1-P3565022.html"
+  },
+  "2416972": {
+    US: "https://www.sephora.com/product/hollywood-flawless-filter-P434104",
+    CA: "https://www.sephora.com/product/hollywood-flawless-filter-P434104?country_switch=ca&lang=en",
+    UK: "https://www.sephora.co.uk/p/charlotte-tilbury-hollywood-flawless-filter",
+    IT: "https://www.sephora.it/p/hollywood-flawless-filter---fluido-perfezionatore-P10014902.html",
+    FR: "https://www.sephora.fr/p/hollywood-flawless-filter---fluide-sublimateur-P10014902.html",
+    ES: "https://www.sephora.es/p/hollywood-flawless-filter---fluido-perfeccionador-P10014902.html",
+    DE: "https://www.sephora.de/p/hollywood-flawless-filter---flusssiges-make-up-P10014902.html",
+    PL: "https://www.sephora.pl/p/hollywood-flawless-filter---podklad-P10014902.html"
+  },
+  "2022416": {
+    US: "https://www.sephora.com/product/protini-tm-polypeptide-cream-P427421",
+    CA: "https://www.sephora.com/product/protini-tm-polypeptide-cream-P427421?country_switch=ca&lang=en",
+    UK: "https://www.sephora.co.uk/p/drunk-elephant-protini-polypeptide-cream-50ml",
+    IT: "https://www.sephora.it/p/protini-polypeptide-cream---crema-idratante-ai-proteini-P3637012.html",
+    FR: "https://www.sephora.fr/p/protini-polypeptide-cream---creme-hydratante-P3637012.html",
+    ES: "https://www.sephora.es/p/protini-polypeptide-cream---crema-hidratante-P3637012.html",
+    DE: "https://www.sephora.de/p/protini-polypeptide-cream---feuchtigkeitscreme-P3637012.html",
+    PL: "https://www.sephora.pl/p/protini-polypeptide-cream---krem-nawilzajacy-P3637012.html"
+  },
+  "1966878": {
+    US: "https://www.sephora.com/product/lip-sleeping-mask-P420652",
+    CA: "https://www.sephora.com/product/lip-sleeping-mask-P420652?country_switch=ca&lang=en",
+    UK: "https://www.sephora.co.uk/p/laneige-lip-sleeping-mask-20g",
+    IT: "https://www.sephora.it/p/lip-sleeping-mask---maschera-notte-labbra-P3703038.html",
+    FR: "https://www.sephora.fr/p/lip-sleeping-mask---masque-de-nuit-l%C3%A8vres-P3703038.html",
+    ES: "https://www.sephora.es/p/lip-sleeping-mask---mascarilla-de-noche-labios-P3703038.html",
+    DE: "https://www.sephora.de/p/lip-sleeping-mask---lippenmaske-P3703038.html",
+    PL: "https://www.sephora.pl/p/lip-sleeping-mask---maseczka-do-ust-P3703038.html"
+  },
+  "1925965": {
+    US: "https://www.sephora.com/product/gloss-bomb-universal-lip-luminizer-P67988452",
+    CA: "https://www.sephora.com/product/gloss-bomb-universal-lip-luminizer-P67988452?country_switch=ca&lang=en",
+    UK: "https://www.sephora.co.uk/p/fenty-beauty-gloss-bomb-universal-lip-luminizer",
+    IT: "https://www.sephora.it/p/gloss-bomb---lucidalabbra-P3075017.html",
+    FR: "https://www.sephora.fr/p/gloss-bomb---brillant-a-levres-P3075017.html",
+    ES: "https://www.sephora.es/p/gloss-bomb---brillo-de-labios-P3075017.html",
+    DE: "https://www.sephora.de/p/gloss-bomb---lipgloss-P3075017.html",
+    PL: "https://www.sephora.pl/p/gloss-bomb---blyszczyk-do-ust-P3075017.html"
+  },
+  "2658821": {
+    US: "https://www.sephora.com/product/glossier-you-eau-de-parfum-P504689",
+    CA: "https://www.sephora.com/product/glossier-you-eau-de-parfum-P504689?country_switch=ca&lang=en",
+    UK: "https://www.sephora.co.uk/p/glossier-you-eau-de-parfum-50ml",
+    IT: "https://www.sephora.it/p/glossier-you---eau-de-parfum-P10052305.html",
+    FR: "https://www.sephora.fr/p/glossier-you---eau-de-parfum-P10052305.html",
+    ES: "https://www.sephora.es/p/glossier-you---eau-de-parfum-P10052305.html",
+    DE: "https://www.sephora.de/p/glossier-you---eau-de-parfum-P10052305.html",
+    PL: "https://www.sephora.pl/p/glossier-you---eau-de-parfum-P10052305.html"
+  },
+  "2181006": {
+    US: "https://www.sephora.com/product/the-dewy-skin-cream-P441101",
+    CA: "https://www.sephora.com/product/the-dewy-skin-cream-P441101?country_switch=ca&lang=en",
+    UK: "https://www.sephora.co.uk/p/tatcha-the-dewy-skin-cream-P1000204066",
+    IT: "https://www.sephora.it/p/the-dewy-skin-cream---crema-idratante-viso-P10041203.html",
+    FR: "https://www.sephora.fr/p/the-dewy-skin-cream---creme-hydratante-P10041203.html",
+    ES: "https://www.sephora.es/p/the-dewy-skin-cream---crema-hidratante-P10041203.html",
+    DE: "https://www.sephora.de/p/the-dewy-skin-cream---gesichtscreme-P10041203.html",
+    PL: "https://www.sephora.pl/p/the-dewy-skin-cream---krem-do-twarzy-P10041203.html"
+  },
+  "2421360": {
+    US: "https://www.sephora.com/product/paulas-choice-skin-perfecting-2-bha-liquid-exfoliant-P469502",
+    CA: "https://www.sephora.com/product/paulas-choice-skin-perfecting-2-bha-liquid-exfoliant-P469502?country_switch=ca&lang=en",
+    UK: "https://www.sephora.co.uk/p/paulas-choice-skin-perfecting-2-bha-liquid-exfoliant-118ml",
+    IT: "https://www.sephora.it/p/skin-perfecting-2-bha-liquid-exfoliant---esfoliante-liquido-P10018804.html",
+    FR: "https://www.sephora.fr/p/skin-perfecting-2-bha-liquid-exfoliant---lotion-exfoliante-P10018804.html",
+    ES: "https://www.sephora.es/p/skin-perfecting-2-bha-liquid-exfoliant---exfoliante-liquido-P10018804.html",
+    DE: "https://www.sephora.de/p/skin-perfecting-2-bha-liquid-exfoliant---flussigpeeling-P10018804.html",
+    PL: "https://www.sephora.pl/p/skin-perfecting-2-bha-liquid-exfoliant---plyn-zloszczajacy-P10018804.html"
+  },
+  "2404846": {
+    US: "https://www.sephora.com/product/glow-recipe-watermelon-glow-niacinamide-dew-drops-P466123",
+    CA: "https://www.sephora.com/product/glow-recipe-watermelon-glow-niacinamide-dew-drops-P466123?country_switch=ca&lang=en",
+    UK: "https://www.sephora.co.uk/p/glow-recipe-watermelon-glow-niacinamide-dew-drops-40ml",
+    IT: "https://www.sephora.it/p/watermelon-glow-niacinamide-dew-drops---siero-viso-P10015606.html",
+    FR: "https://www.sephora.fr/p/watermelon-glow-niacinamide-dew-drops---serum-visage-P10015606.html",
+    ES: "https://www.sephora.es/p/watermelon-glow-niacinamide-dew-drops---suero-facial-P10015606.html",
+    DE: "https://www.sephora.de/p/watermelon-glow-niacinamide-dew-drops---gesichtsserum-P10015606.html",
+    PL: "https://www.sephora.pl/p/watermelon-glow-niacinamide-dew-drops---serum-do-twarzy-P10015606.html"
+  },
+  "2334860": {
+    US: "https://www.sephora.com/product/summer-fridays-lip-butter-balm-P455936",
+    CA: "https://www.sephora.com/product/summer-fridays-lip-butter-balm-P455936?country_switch=ca&lang=en",
+    UK: "https://www.sephora.co.uk/p/summer-fridays-lip-butter-balm",
+    IT: "https://www.sephora.it/p/lip-butter-balm---balsamo-labbra-P10016301.html",
+    FR: "https://www.sephora.fr/p/lip-butter-balm---baume-a-levres-P10016301.html",
+    ES: "https://www.sephora.es/p/lip-butter-balm---balsamo-de-labios-P10016301.html",
+    DE: "https://www.sephora.de/p/lip-butter-balm---lippenbalsam-P10016301.html",
+    PL: "https://www.sephora.pl/p/lip-butter-balm---balsam-do-ust-P10016301.html"
+  }
+};
+
 export async function scrapeSephoraLocalized(countryCode = "US") {
   const code = (countryCode || "US").toUpperCase();
   const cfg = SEPHORA_JS_CONFIG[code] || SEPHORA_JS_CONFIG.US;
@@ -34,9 +157,8 @@ export async function scrapeSephoraLocalized(countryCode = "US") {
   for (const item of BASE_PRODUCTS) {
     const localPrice = Math.round(item.usdPrice * cfg.rate * 100) / 100;
     const origPrice = Math.round(localPrice * 1.18 * 100) / 100;
-    const prodUrl = cfg.domain.includes("sephora.com")
-      ? `https://www.sephora.com/product/${item.name.toLowerCase().replace(/[^a-z0-9]+/g, "-")}-P${item.skuId}`
-      : `https://www.${cfg.domain}/shop/beauty/${item.name.toLowerCase().replace(/[^a-z0-9]+/g, "-")}-${item.skuId}.html`;
+    const exactUrls = EXACT_PRODUCT_URLS[item.skuId] || {};
+    const prodUrl = exactUrls[code] || exactUrls.US || `https://www.${cfg.domain}`;
 
     products.push({
       name: item.name,
